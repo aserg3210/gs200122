720ebb6f5197357d0c1af6bd9d03254f75576273
aserg3210@gmail.comm <aserg3210@gmail.com>
2022-01-21 01:39:04 +0300

    base 1

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using Core.Interfaces;
 
 namespace API
 {
@@ -29,7 +31,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
 
             services.AddDbContext<StoreContext>(x=>
