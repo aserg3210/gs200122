0831157b64a43a89b55436e109c16e0b5c8cbf3a
aserg3210@gmail.comm <aserg3210@gmail.com>
2022-02-03 04:51:45 +0300

    client: base routing was added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { ShopRoutingModule } from './shop/shop-routing.module';
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'shop', component: ShopComponent},
-  {path: 'shop/:id', component: ProductDetailsComponent},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},//lazy
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
