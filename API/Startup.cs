ea8d95873e8411ad635b5386abeadcfaaca20551
aserg3210@gmail.comm <aserg3210@gmail.com>
2022-02-04 06:17:37 +0300

    basket repository, controller, redis were added

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,6 +20,7 @@ using API.Helpers;
 using API.Middleware;
 using API.Errors;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -38,12 +39,14 @@ namespace API
         {
 
             services.AddAutoMapper(typeof(MappingProfiles));
-
             services.AddControllers();
-
             services.AddDbContext<StoreContext>(x=>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+                {
+                    var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                    return ConnectionMultiplexer.Connect(configuration);
+                });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
