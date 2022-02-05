1dca1508f5f29a546d205c4d0c43e363ea5ca8ca
aserg3210@gmail.comm <aserg3210@gmail.com>
2022-02-05 08:11:11 +0300

    API validation of auth, basket were added

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
-﻿using Core.Entities;
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +13,12 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository basketRepository)
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
             _basketRepository = basketRepository;
+            _mapper = mapper;
         }
         [HttpGet]
         public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
@@ -23,9 +27,10 @@ namespace API.Controllers
             return Ok(basket ?? new CustomerBasket(id));
         }
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasketDto>> UpdateBasket(CustomerBasketDto basket)
         {
-            var updateBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var updateBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
             return Ok(updateBasket);
         }
         [HttpDelete]
--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+            ErrorMessage = "Пароль должен содержать как минимум 6 символов: 1 заглавную букву, 1 маленькую букву, 1 цифру, 1 символ(не цифра и не буква)")]
         public string Password { get; set; }
     }
 }
