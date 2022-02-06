67497f6cdef53df89eb5485d26316a8f05a34721
aserg3210@gmail.comm <aserg3210@gmail.com>
2022-02-06 09:12:51 +0300

    client identification, validation; routing guard were added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 
@@ -10,9 +11,22 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'GSShop';
 
-  constructor(private basketService: BasketService){}
+  constructor(private basketService: BasketService, private accountService: AccountService){}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    //if(token) {
+      this.accountService.loadCurrentUser(token).subscribe({
+        next: () => console.log('loaded user'),
+        error: (e) => console.log(e)
+      });
+    //}
+  }
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe({
